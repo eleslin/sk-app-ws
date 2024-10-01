5c064441a10f87f2e896ed09cc18b297a1c63362
develoba <srta.scobar@gmail.com>
2024-10-01 07:32:38 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts", // where the service worker src is
+  swSrc: "src/app/sw.ts", // where the service worker src is
   swDest: "public/sw.js", // where the service worker code will end up
 });
 
