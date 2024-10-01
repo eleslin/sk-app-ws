9db6c1177eee2986e5b48bd025007046cfc6b153
develoba <srta.scobar@gmail.com>
2024-10-01 07:15:29 +0200

    try new project

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next PWA with Serwist Example',
+    short_name: 'My PWA',
+    description: 'An example of how to use Serwist in Next.js',
+    start_url: '/',
+    display: 'standalone',
+    orientation: 'portrait',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  };
+}
